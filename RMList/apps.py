c3f1d5fbd1e383a0f36f8ef77b609a0602a1fc82
Ricca <mique.riccamae.ictb@gmail.com>
2021-04-06 20:25:59 +0800

    This is my First Commit for Project RiccaProject

---
+++ RMList/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RmlistConfig(AppConfig):
+    name = 'RMList'
---
+++ RMList/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import resolve
+from django.test import TestCase
+from CTList.views import MainPage
+class HomePageTest(TestCase):
+   def test_root_url_resolves_to_mainpage_view(self):
+      found = resolve('/')
+      self.assertEqual(found.func, MainPage)
+
+
