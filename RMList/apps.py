3e3bf02b3a4ef96a1fe9e70c081df11b0a627b12
Ricca <mique.riccamae.ictb@gmail.com>
2021-04-07 22:33:27 +0800

    FUNCTIONAL COMMIT

--- RMList/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RmlistConfig(AppConfig):
-    name = 'RMList'
--- test.py
+++ test.py
@@ -1,3 +1,25 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:9900')
+import unittest
+
+class Test(unittest.TestCase):  #1
+
+    
+ def setUp(self):
+    self.browser = webdriver.Firefox()
+    self.browser.implicitly_wait(3)
+
+ def tearDown(self):
+
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)  #5
+        self.fail('Finish the test!')  #6
+
+        # She is invited to enter a to-do item straight away
+   
+
+if __name__ == '__main__':  #7
+    unittest.main(warnings='ignore')  #
